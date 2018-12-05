685e393149079eab63f7536457d8c89470a3b8bb
Luiz Pablo <lpablotavares@gmail.com>
2018-12-05 00:03:11 -0300

    change ip address

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://10.0.0.106:5000' });
+const api = axios.create({ baseURL: 'http://localhost:5000' });
 
 export default api;
